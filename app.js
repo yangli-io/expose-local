ed245bfc015bb2bd2a1b18656b1fda88dc8a74eb
Yang Li <yangli@Yangs-Mac-mini.local>
2015-04-15 13:35:33 +1000

    yooo

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 var http = require('http');
 
+var queue = []
+var count = 0;
+
 http.createServer(function(req, res){
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.write(getIP(req));
-	res.end();
+	if (req.url != '/get'){
+			queue[count] = {
+			url: req.url,
+			data: ""
+		}
+		queue[count].watch('data', function(id, oldVal, newVal){
+			res.write(newVal);
+			res.end();
+			this.unwatch('data');
+		})
+		count++;
+	} else {
+		res.write(JSON.stringify(queue, true));
+		res.end();
+	}
+
 }).listen(process.env.PORT || 9000)
 
 
 function getIP(req){
 	var ip = {
-		
 		connection: {
 			remoteAddress: req.connection && req.connection.remoteAddress,
 			socket: {
@@ -22,4 +38,50 @@ function getIP(req){
 	}
 	ip['x-forwarded-for'] = req.headers && req.headers['x-forwarded-for'];
 	return JSON.stringify(ip, true);
+}
+
+
+
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+ 
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
 }
