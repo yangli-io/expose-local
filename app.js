fbed690cebcec3acce9cdd82a7abc03fd778455d
Yang Li <yangli@Yangs-Mac-mini.local>
2015-04-15 11:45:37 +1000

    remove fs

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 var http = require('http');
-var fs = require('fs');
-
-var log = fs.readFileSync('./log.txt');
 
 http.createServer(function(req, res){
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-	console.log(ip);
-	log += ip + "\n";
-	fs.writeFileSync('./log.txt', log);
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.write("yolo");
+	res.write(ip);
 	res.end();
 }).listen(process.env.PORT || 9000)
