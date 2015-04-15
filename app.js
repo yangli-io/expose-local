14a7efd5f501df8c64246887e8e07bf639e5ea9a
Yang Li <yangli@Yangs-Mac-mini.local>
2015-04-15 11:47:52 +1000

    all address

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+	var ip = req.headers['x-forwarded-for'] + "||" + req.connection.remoteAddress + "||" + req.socket.remoteAddress + "||" + req.connection.socket.remoteAddress;
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
 	res.write(ip);
 	res.end();
